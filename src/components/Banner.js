186925e61edfb4920b6f0625b1e3a994be3a4831
saaad99 <ehdgnstla@naver.com>
2024-12-06 19:22:12 +0900

    [add] isClicked useState추가로 banner의 play 클릭 시 화면 전환 구현

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,12 +4,14 @@ import axios from '../api/axios';
 import './Banner.css';
 export default function Banner() {
     const [movie, setMovie] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
 
     useEffect(() => {
         fetchData();
     }, [])
 
 
+    
     const fetchData = async () => {
         //현재 상영 중인 영화 정보를 가져오기(여러 영화)
         const request = await axios.get(requests.fetchNowPlaying);
@@ -32,27 +34,37 @@ export default function Banner() {
     const truncate = (str, n) => {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     }
-    return (
-        <header
-            className='banner'
-            style={{
-                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'top center',
-            }}
-        >
-            <div className='banner__contents'>
-                {/* movie 타이틀이 없다면 name, name이 없다면 original_name */}
-                <h1 className='banner__title' >{movie.title || movie.name || movie.original_name}</h1>
 
-                <div className='banner_buttons'>
-                    <button className='banner_button play'>Play</button>
-                    <button className='banner_button info'>More Information</button>
+    if (!isClicked) {     
+        return (
+            <header
+                className='banner'
+                style={{
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'top center',
+                }}
+            >
+                <div className='banner__contents'>
+                    {/* movie 타이틀이 없다면 name, name이 없다면 original_name */}
+                    <h1 className='banner__title' >{movie.title || movie.name || movie.original_name}</h1>
+    
+                    <div className='banner_buttons'>
+                        <button className='banner_button play' onClick={() => setIsClicked(true)}>Play</button>
+                        <button className='banner_button info'>More Information</button>
+                    </div>
+    
+                    <h1 className='banner__description'>{truncate(movie.overview, 100)}</h1>
                 </div>
-
-                <h1 className='banner__description'>{truncate(movie.overview, 100)}</h1>
+                <div className='banner_fadeBottom'/>
+            </header>
+        )
+    } else {
+        return (
+            <div>
+                clicked
             </div>
-            <div className='banner_fadeBottom'/>
-        </header>
-    )
+        )
+    }
+
 }
