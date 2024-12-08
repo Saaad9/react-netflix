d89fed99e08d872d81dde42412c35c02c897895e
saaad99 <ehdgnstla@naver.com>
2024-12-08 20:27:03 +0900

    [add] detail Page backdrop_path 이미지 띄우기

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from '../../api/axios';
 
 export default function DetailPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const {movieId} = useParams();
+  const [movie, setMovie] = useState({});
+  console.log(movieId);
+
+  useEffect(() => {
+    async function fecthData() {
+      const request = await axios.get(
+        `/movie/${movieId}`
+      )
+      setMovie(request.data);
+    }
+    fecthData();
+  },[])
+
+  if(!movie) return <div>...loading</div>;
+
+  return <section>
+    <img
+      className='modal__poster-img'
+      src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+      alt='poster'
+    ></img>
+  </section>
 }
